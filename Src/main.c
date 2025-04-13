28f9666a51105c370b1405e903d0ae208d713851
Timothy <timothyfelt@techiebill.com>
2025-04-12 20:22:46 -0600

    First Test.

--- Src/main.c
+++ Src/main.c
@@ -5,18 +5,6 @@ int main(void)
 {
   #if defined(LAB1)
   lab1_main();
-  #elif defined(LAB2)
-  lab2_main();
-  #elif defined(LAB3)
-  lab3_main();
-  #elif defined(LAB4)
-  lab4_main();
-  #elif defined(LAB5)
-  lab5_main();
-  #elif defined(LAB6)
-  lab6_main();
-  #elif defined(LAB7)
-  lab7_main();
   #else
   #error No valid target specified
   #endif
